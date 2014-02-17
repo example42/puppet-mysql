65cb385db29eaeca28188da2f118a6c472300c9c
Alessandro Franceschi <al@lab42.it>
2014-02-17 16:47:20 +0100

    Fixing spec tests for rspec-puppet 2.x

--- spec/classes/mysql_spec.rb
+++ spec/classes/mysql_spec.rb
@@ -26,14 +26,8 @@ describe 'mysql' do
     it { should contain_service('mysql').with_ensure('running') }
     it { should contain_service('mysql').with_enable('true') }
     it { should contain_file('mysql.conf').with_ensure('present') }
-    it 'should monitor the process' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == true
-    end
-    it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('true') }
+    it { should contain_firewall('mysql_tcp_42').with_enable('true') }
   end
 
   describe 'Test decommissioning - absent' do
@@ -42,18 +36,9 @@ describe 'mysql' do
     it 'should remove Package[mysql]' do should contain_package('mysql').with_ensure('absent') end
     it 'should not define Service[mysql]' do should_not contain_service('mysql') end
     it 'should remove mysql configuration file' do should contain_file('mysql.conf').with_ensure('absent') end
-    it 'should not automatically restart the service, when absent => true' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:notify]
-      content.should be_nil
-    end
-    it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:enable]
-      content.should == false
-    end
+    it { should contain_file('mysql.conf').without_notify }
+    it { should contain_monitor__process('mysql_process').with_enable('false') }
+    it { should contain_firewall('mysql_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disable' do
@@ -63,14 +48,8 @@ describe 'mysql' do
     it 'should stop Service[mysql]' do should contain_service('mysql').with_ensure('stopped') end
     it 'should not enable at boot Service[mysql]' do should contain_service('mysql').with_enable('false') end
     it { should contain_file('mysql.conf').with_ensure('present') }
-    it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:enable]
-      content.should == false
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('false') }
+    it { should contain_firewall('mysql_tcp_42').with_enable('false') }
   end
 
   describe 'Test decommissioning - disableboot' do
@@ -81,153 +60,88 @@ describe 'mysql' do
     it { should_not contain_service('mysql').with_ensure('absent') }
     it 'should not enable at boot Service[mysql]' do should contain_service('mysql').with_enable('false') end
     it { should contain_file('mysql.conf').with_ensure('present') }
-    it 'should not monitor the process locally' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should keep a firewall rule' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('false') }
+    it { should contain_firewall('mysql_tcp_42').with_enable('true') }
   end
 
   describe 'Test customizations - template' do
     let(:params) { {:template => "mysql/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
 
-    it 'should generate a valid template' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
-    end
-    it 'should generate a template that uses custom options' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:content]
-      content.should match "value_a"
-    end
-
+    it { should contain_file('mysql.conf').with_content(/fqdn: rspec.example42.com/) }
+    it { should contain_file('mysql.conf').with_content(/value_a/) }
   end
 
   describe 'Test customizations - source' do
     let(:params) { {:source => "puppet://modules/mysql/spec" , :source_dir => "puppet://modules/mysql/dir/spec" , :source_dir_purge => true } }
 
-    it 'should request a valid source ' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:source]
-      content.should == "puppet://modules/mysql/spec"
-    end
-    it 'should request a valid source dir' do
-      content = catalogue.resource('file', 'mysql.dir').send(:parameters)[:source]
-      content.should == "puppet://modules/mysql/dir/spec"
-    end
-    it 'should purge source dir if source_dir_purge is true' do
-      content = catalogue.resource('file', 'mysql.dir').send(:parameters)[:purge]
-      content.should == true
-    end
+    it { should contain_file('mysql.conf').with_source('puppet://modules/mysql/spec') }
+    it { should contain_file('mysql.dir').with_source('puppet://modules/mysql/dir/spec') }
+    it { should contain_file('mysql.dir').with_purge('true') }
   end
 
   describe 'Test customizations - custom class' do
     let(:params) { {:my_class => "mysql::spec" } }
-    it 'should automatically include a custom class' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
-    end
+    it { should contain_file('mysql.conf').with_content(/fqdn: rspec.example42.com/) }
   end
 
   describe 'Test service autorestart' do
-    it 'should automatically restart the service, by default' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:notify]
-      content.should == "Service[mysql]"
-    end
+    it { should contain_file('mysql.conf').with_notify('Service[mysql]') }
   end
 
   describe 'Test service autorestart' do
     let(:params) { {:service_autorestart => "no" } }
-
-    it 'should not automatically restart the service, when service_autorestart => false' do
-      content = catalogue.resource('file', 'mysql.conf').send(:parameters)[:notify]
-      content.should be_nil
-    end
+    it { should contain_file('mysql.conf').without_notify('Service[mysql]') }
   end
 
   describe 'Test Puppi Integration' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
-
-    it 'should generate a puppi::ze define' do
-      content = catalogue.resource('puppi::ze', 'mysql').send(:parameters)[:helper]
-      content.should == "myhelper"
-    end
+    it { should contain_puppi__ze('mysql').with_helper('myhelper') }
   end
 
   describe 'Test Monitoring Tools Integration' do
     let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
-
-    it 'should generate monitor defines' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:tool]
-      content.should == "puppi"
-    end
+    it { should contain_monitor__process('mysql_process').with_tool('puppi') }
   end
 
   describe 'Test Firewall Tools Integration' do
     let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
-
-    it 'should generate correct firewall define' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
-    end
+    it { should contain_firewall('mysql_tcp_42').with_tool('iptables') }
   end
 
   describe 'Test OldGen Module Set Integration' do
     let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" } }
 
-    it 'should generate monitor resources' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:tool]
-      content.should == "puppi"
-    end
-    it 'should generate firewall resources' do
-      content = catalogue.resource('firewall', 'mysql_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
-    end
-    it 'should generate puppi resources ' do
-      content = catalogue.resource('puppi::ze', 'mysql').send(:parameters)[:ensure]
-      content.should == "present"
-    end
+    it { should contain_monitor__process('mysql_process').with_tool('puppi') }
+    it { should contain_firewall('mysql_tcp_42').with_tool('iptables') }
+    it { should contain_puppi__ze('mysql').with_ensure('present') }
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
 
-    it 'should honour top scope global vars' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('true') }
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :mysql_monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
 
-    it 'should honour module specific vars' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('true') }
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :mysql_monitor => true , :ipaddress => '10.42.42.42' } }
     let(:params) { { :port => '42' } }
 
-    it 'should honour top scope module specific over global vars' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('true') }
   end
 
   describe 'Test params lookup' do
     let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42' } }
     let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
 
-    it 'should honour passed params over global vars' do
-      content = catalogue.resource('monitor::process', 'mysql_process').send(:parameters)[:enable]
-      content.should == true
-    end
+    it { should contain_monitor__process('mysql_process').with_enable('true') }
   end
 
 end
