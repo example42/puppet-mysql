a0057c65e20918916a5aa3bf791913f83d341503
BlurryFlurry <chan@elakiri.com>
2015-11-19 13:08:24 +0530

    Fixed the error by removing escape character.
    http://i.imgur.com/qaFUlGv.png?1
    
    ' in file /tmp/vagrant-puppet/modules-76bee6c9d10deeda599d20f2249d671a/mysql/manifests/queryfile.pp at line 45
    ' in file /tmp/vagrant-puppet/modules-76bee6c9d10deeda599d20f2249d671a/mysql/manifests/queryfile.pp at line 46
    ' in file /tmp/vagrant-puppet/modules-76bee6c9d10deeda599d20f2249d671a/mysql/manifests/queryfile.pp at line 47
    
    https://github.com/example42/puppet-mysql/blob/master/manifests/queryfile.pp#L44-L46

--- manifests/queryfile.pp
+++ manifests/queryfile.pp
@@ -41,10 +41,7 @@ define mysql::queryfile (
   }
 
   exec { "mysqlqueryfile-${name}":
-    command => "mysql ${arg_mysql_defaults_file} \
-                ${arg_mysql_user} ${arg_mysql_password} ${arg_mysql_host} \
-                ${mysql_db} < ${mysql_file} && \
-                touch ${mysql_query_filepath}/mysqlqueryfile-${name}.run",
+    command => "mysql ${arg_mysql_defaults_file} ${arg_mysql_user} ${arg_mysql_password} ${arg_mysql_host} ${mysql_db} < ${mysql_file} && touch ${mysql_query_filepath}/mysqlqueryfile-${name}.run",
     path    => [ '/usr/bin' , '/usr/sbin' , '/bin' , '/sbin' ],
     creates => "${mysql_query_filepath}/mysqlqueryfile-${name}.run",
     unless  => "ls ${mysql_query_filepath}/mysqlqueryfile-${name}.run",
